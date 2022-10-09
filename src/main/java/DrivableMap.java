ff0db7d6a38697e7739520f1afc881b34373abc6
Oswin Gan <oswin.gan@gmail.com>
2022-10-09 15:39:03 -0400

    attempt 1

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -28,7 +28,13 @@ class DrivableMap {
      *       Return true if the Drivable was added to drivable_map.
      */
 
-
+    public boolean addDrivable(String ID, Drivable object){
+        if (!drivable_map.containsKey(ID)){
+            drivable_map.put(ID, object);
+            return true;
+        }
+        return false;
+    }
 
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
@@ -38,7 +44,14 @@ class DrivableMap {
      * iterate through drivable_map.
      */
 
-
+    public boolean hasFasterThan(int speed){
+        for(Drivable value: drivable_map.values()){
+            if(value.getMaxSpeed() >= speed){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -47,7 +60,15 @@ class DrivableMap {
      *       drivable_map.
      */
 
-
+    public List getTradable(){
+        List<Tradable> tradableList = new ArrayList<>();
+        for(Object value: drivable_map.values()){
+            if (value instanceof Tradable){
+                tradableList.add((Tradable) value);
+            }
+        }
+        return tradableList;
+    }
 
     
 }
--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -10,6 +10,7 @@
  * Go through the TODOs below and complete them.
  */
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +40,11 @@ public class Trader<T> {
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
-
+    public Trader(int money){
+        this.money = money;
+        this.inventory = new ArrayList<>();
+        this.wishlist = new ArrayList<>();
+    }
 
 
 
@@ -48,7 +53,9 @@ public class Trader<T> {
      *       object of type T and adds it to this Trader's wishlist.
      */
 
-
+    public void addToWishlist(T item){
+        this.wishlist.add(item);
+    }
 
 
 
@@ -60,7 +67,12 @@ public class Trader<T> {
      *       We will call this in exchangeMoney().
      */
 
-
+    public int getSellingPrice(T item){
+        if (item instanceof Tradable){
+            return ((Tradable) item).getPrice();
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
 
 
